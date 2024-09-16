ae7335802da6636eafc9345c72fb3583ef67e39a
kaster-zachary <zckaster@sep.com>
2024-09-16 18:33:51 -0400

    add template project for maui blazor hybrid

---
+++ SkillJourney.Client.Mobile/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SkillJourney.Client.Mobile;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ SkillJourney.Client.Mobile/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SkillJourney.Client.Mobile;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
